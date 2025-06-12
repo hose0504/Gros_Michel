755d0c3946ba433caff037d63b06fb5fb8b3896b
wish <hose0504@naver.com>
2025-06-12 14:03:42 +0900

    061204 apply

--- Terraform/modules/network/outputs.tf
+++ Terraform/modules/network/outputs.tf
@@ -14,3 +14,10 @@ output "private_route_table_id" {
   value = aws_route_table.private.id
 }
 
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
