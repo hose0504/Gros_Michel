e65cf067ad0ea404ce5571555d6bd720aa77ce32
kyh <oyohee1@gmail.com>
2025-06-12 16:21:09 +0900

    apply

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -56,8 +56,8 @@ module "eks" {
   source          = "./modules/eks"
   cluster_name    = "gros-michel-eks"
   cluster_version = "1.32"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.network.vpc_id
+  subnet_ids      = module.network.private_subnets
 }
 
 module "nat_instance" {
