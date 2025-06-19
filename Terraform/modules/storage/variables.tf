86c2eacff934620ae27add0b8e42b3d7a1034dc4
kyh <oyohee1@gmail.com>
2025-06-19 13:59:29 +0900

    apply

--- Terraform/modules/storage/variables.tf
+++ Terraform/modules/storage/variables.tf
@@ -2,7 +2,3 @@ variable "environment" {
   description = "The environment name (e.g., dev, staging, prod)"
   type        = string
 }
-
-output "bucket_domain_name" {
-  value = aws_s3_bucket.shared.bucket_regional_domain_name
-}
