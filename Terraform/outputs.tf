f6964a8d8cce1d6bebb831c8f9a949ec5c9ea2ce
unknown <hose0504@naver.com>
2025-07-12 20:32:36 +0900

    destroy

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   value = module.network.vpc_id
 }
 output "private_subnet_ids" {
-  value = module.network.private_subnets
+  value = module.network.private_subnet_ids
 }
 output "public_subnet_ids" {
   value = module.network.public_subnet_ids
