4637353128f1aa35cfa5a0e4e64fe4237190cb03
unknown <hose0504@naver.com>
2025-07-13 15:13:25 +0900

    destroy

--- Terraform/modules/nat_instance/variables.tf
+++ Terraform/modules/nat_instance/variables.tf
@@ -4,6 +4,3 @@ variable "nat_instance_type" { default = "t3.micro" }
 variable "ami_id" { default = "ami-01ad0c7a4930f0e43" }
 variable "key_name" { default = null }
 
-output "nat_instance_eni" {
-  value = aws_instance.nat.primary_network_interface_id
-}
