4eb5167e662361c001b423af6c9f17b02a2aee09
unknown <hose0504@naver.com>
2025-07-13 15:15:06 +0900

    destroy

--- Terraform/modules/nat_instance/outputs.tf
+++ Terraform/modules/nat_instance/outputs.tf
@@ -2,10 +2,6 @@ output "nat_instance_id" {
   value = aws_instance.nat_instance.id
 }
 
-output "nat_instance_private_ip" {
-  value = aws_instance.nat_instance.private_ip
-}
-
 output "nat_instance_eni" {
   value = aws_instance.nat_instance.primary_network_interface_id
 }
