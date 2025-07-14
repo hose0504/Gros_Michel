5235b3b4da64a383f2b07263e567de3722828ace
unknown <hose0504@naver.com>
2025-07-14 14:49:34 +0900

    apply

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -32,10 +32,10 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = var.vpc_id
-  subnet_ids      = var.private_subnets
+  subnet_ids      = var.public_subnets
 
   cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = false
+  cluster_endpoint_private_access = true
   enable_irsa                     = true
 
   eks_managed_node_groups = {
